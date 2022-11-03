47b84fdae269ce96dd301b8fd079d6e208963249
Ganiyu Wareez <ganiyuwareez2017@gmail.com>
2022-11-03 13:59:15 +0100

    update

--- script.js
+++ script.js
@@ -12,3 +12,20 @@ document.querySelectorAll('.links').forEach((i) =>
     navMenu.classList.remove('active');
   })
 );
+
+const productContainers = [...document.querySelectorAll('.product-container')];
+const nextBtn = [...document.querySelectorAll('.next-btn')];
+const preBtn = [...document.querySelectorAll('.pre-btn')];
+
+productContainers.forEach((item, i) => {
+  let containerDimenstions = item.getBoundingClientRect();
+  let containerWidth = containerDimenstions.width;
+
+  nextBtn[i].addEventListener('click', () => {
+    item.scrollLeft += containerWidth;
+  });
+
+  preBtn[i].addEventListener('click', () => {
+    item.scrollLeft -= containerWidth;
+  });
+});
